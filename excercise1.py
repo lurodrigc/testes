dd909c6b5d9c02a2770779b0cb48611a37733cc7
Luiza <lurodrigc@gmail.com>
2025-10-05 16:39:16 -0300

    Add files via upload

---
+++ excercise1.py
@@ -0,0 +1,12 @@
+# here im creating programs based on the site practicepython.com (this is the first one)
+
+name = input("Qual seu nome? ")
+print("Olá,", name, "!")
+age = int(input("Quantos anos você tem? "))
+print(age, "! Vamos ver em que ano você terá 100 anos!")
+
+birthyear = 2025 - age
+print("Bom, você nasceu em", birthyear, "...")
+
+final = birthyear + 100
+print("Bom! Você terá 100 anos em:", final)
